a8c84245e75f48283e0307117d8ee283eb59b874
BhoumikPatidar <bhoumikmnc@gmail.com>
2025-02-14 18:00:41 +0530

    changes to make it more accessible

--- server/src/app.js
+++ server/src/app.js
@@ -20,6 +20,11 @@ app.use('/api/codes', codeRoutes);
 app.use('/api/comments', commentRoutes);
 
 // Sync database and start the server
-syncDatabase().then(() => {
-  app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-}).catch(err => console.error("Error syncing database:", err));
+syncDatabase()
+  .then(() => {
+    app.listen(process.env.PORT, '0.0.0.0', () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error("Error syncing database:", err));
+
