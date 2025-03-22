a410df54353d657cb138010cc92a83082247dfdc
BhoumikPatidar <bhoumikmnc@gmail.com>
2025-03-22 21:15:30 +0530

    fix the issue of frontend not running from other devices by fixing URL as the server URL in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,5 @@
 import axios from 'axios';
 
-// Get the GitHub Codespace URL from the window location
-const isCodespace = window.location.hostname.includes('.app.github.dev');
-// const baseURL = isCodespace 
-//   ? `https://${window.location.hostname.replace('5173', '80')}/api`
-//   : 'http://127.0.0.1:80/api';
-
 const baseURL = 'http://3.82.96.157:5000/api';
 
 const api = axios.create({
