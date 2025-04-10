a54693e2db72e7e26b1955e6d2b8565fcce820c5
dewanshkumar123 <dewansh.kumar@iitgn.ac.in>
2025-04-10 17:15:03 +0530

    l

--- server/src/routes/repoRoutes.js
+++ server/src/routes/repoRoutes.js
@@ -1,7 +1,13 @@
-// server/src/routes/repoRoutes.js
 const express = require("express");
 const router = express.Router();
-const { listRepos, getCommits, getDiff, createRepo, getRepoTree, getFileContent } = require("../controllers/repoController");
+const { 
+  listRepos, 
+  createRepo, 
+  getCommits, 
+  getDiff,
+  getRepoTree,
+  getFileContent 
+} = require("../controllers/repoController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Endpoint to list all repositories
@@ -16,8 +22,10 @@ router.get("/:repoName/commits", getCommits);
 // Endpoint to get diff for a specific commit in a repository
 router.get("/:repoName/diff/:commitSha", getDiff);
 
-router.get("/:repoName/tree", getRepoTree);     // GET /repos/:repoName/tree?path={optional}
-router.get("/:repoName/file", getFileContent);    // GET /repos/:repoName/file?path={filePath}
+// Endpoint to get repository tree
+router.get("/:repoName/tree", getRepoTree);
 
+// Endpoint to get file content
+router.get("/:repoName/file", getFileContent);
 
 module.exports = router;
