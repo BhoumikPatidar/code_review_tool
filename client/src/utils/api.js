810b3d3f48f0c0266fa7b93179e081ae75cf0d65
dewanshkumar123 <dewansh.kumar@iitgn.ac.in>
2025-04-16 04:33:29 +0530

    k

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const baseURL = 'http://65.2.3.200:5000/api';
 
 const api = axios.create({
-  baseURL: baseURL
+  baseURL: 'http://65.2.3.200:5000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 // Create a function to setup the token that can be called on login
