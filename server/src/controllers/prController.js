59a14cb59b2ff8881c8a799e13ecb0049abf8972
dewanshkumar123 <dewansh.kumar@iitgn.ac.in>
2025-04-16 21:24:31 +0530

    prev

--- server/src/controllers/prController.js
+++ server/src/controllers/prController.js
@@ -32,7 +32,6 @@ function execPromise(command) {
  */
 const createPR = async (req, res) => {
   try {
-    console.log(req.user);
     const { repository, sourceBranch, targetBranch, title, description } = req.body;    
     const pr = await PullRequest.create({
       repository,
